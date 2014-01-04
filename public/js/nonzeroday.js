6fdec27e4dc02e27346fa7021c88c89de5ba37b0
Wil Yegelwel <wyegelwel@gmail.com>
2014-01-03 20:41:48 -0500

    Clicking checkbox now alters data stored

--- public/js/nonzeroday.js
+++ public/js/nonzeroday.js
@@ -1,24 +1,31 @@
 var D = {};
 
+function toggleGoalCompletion(goal){
+	D.goals[D.dayShowing][goal].completed = !D.goals[D.dayShowing][goal].completed;
+}
+
 function displayGoal(goal, container){
 	goalWrapper = $("<div>");	
 	label = $("<label>").addClass("goal").text(goal.text).appendTo(goalWrapper);
-	$("<input type='checkbox' />").addClass("goalCheck").appendTo(label);
+	$("<input type='checkbox' />").addClass("goalCheck")
+								  .appendTo(label)
+								  .attr("name", goal.text)
+								  .prop("checked", goal.completed)
+								  .click(function(e){
+								  		toggleGoalCompletion($(this).attr("name"));
+								  });
 	goalWrapper.appendTo(container);
 }
 
 function displayGoals(goals){
 	goalContainer = $("#createdGoalsContainer");
-	if (goals.length == 0){
+	if (Object.keys(goals).length == 0){
 		// $("<div>").text("You should add some goals").appendTo(goalContainer);
-		
 	}else{
-		$.each(goals, function(i, goal){
-			displayGoal(goal, goalContainer);
-		});
+		for (var goal in goals){
+			displayGoal(goals[goal], goalContainer);
+		}
 	}
-	
-	
 }
 
 function clearGoals(){
@@ -31,21 +38,25 @@ $("#addBtn").click(function(){
 	console.log(goalText)
 	if (goalText.length > 0){
 		goal = {text: goalText, complete:false}
-		D.goals.push(goal);
+		D.goals[D.dayShowing][goalText] = goal;
 		clearGoals();
-		displayGoals(D.goals);
+		displayGoals(D.goals[D.dayShowing]);
 		goalBox.val('');
 	}
 });
 
 
 function onStartUp(){
-	D.goals = [
-				{text: "Finish App",
-				 complete: false
+	D.goals = {"today":
+				{ "Finish App":
+					{text: "Finish App",
+					 completed: true
+					}
 				}	
-				];
-	displayGoals(D.goals);
+			  };
+	D.dayShowing = "today";
+	console.log(D.goals[D.dayShowing]);
+	displayGoals(D.goals[D.dayShowing]);
 }
 
 onStartUp();
